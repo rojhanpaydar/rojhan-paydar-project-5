1e2392ebd8ec74234a310147141e5e76316efa5d
Rojhan Paydar <rojhanpaydar@gmail.com>
2020-06-23 12:18:31 -0400

    Updates

---
+++ precache-manifest.0279ab6fe1566be843671f18efd43c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36627c31a105f6a33fe84e1b7a9858c3",
+    "url": "/rojhan-paydar-project-5/index.html"
+  },
+  {
+    "revision": "6304278344e69686ee90",
+    "url": "/rojhan-paydar-project-5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0212ebd21539545e8a70",
+    "url": "/rojhan-paydar-project-5/static/js/2.71256099.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rojhan-paydar-project-5/static/js/2.71256099.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6304278344e69686ee90",
+    "url": "/rojhan-paydar-project-5/static/js/main.3550a500.chunk.js"
+  },
+  {
+    "revision": "df064b5c09af5da013c8",
+    "url": "/rojhan-paydar-project-5/static/js/runtime-main.2b7849a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rojhan-paydar-project-5/static/media/logo.5d5d9eef.svg"
+  }
+]);
