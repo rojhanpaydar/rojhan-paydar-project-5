b65f8a9b1bfd1e19f1ae0e92c7329c0aa4425597
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-04 15:08:44 -0500

    Updates

--- precache-manifest.5a7069287584d89970b60d263ef755a1.js
+++ precache-manifest.5a7069287584d89970b60d263ef755a1.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "929a5cff32b1177f9efd894cf6df83fc",
+    "revision": "4c86a455544132259f70a913cd1aade3",
     "url": "/rojhan-paydar-project-5/index.html"
   },
   {
-    "revision": "0bf6ebf22de1143162ca",
-    "url": "/rojhan-paydar-project-5/static/css/main.aa34b28a.chunk.css"
+    "revision": "dd621300fa17479c791f",
+    "url": "/rojhan-paydar-project-5/static/css/main.966591b9.chunk.css"
   },
   {
     "revision": "35770f317209c999c9f6",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/rojhan-paydar-project-5/static/js/2.7d2715d2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0bf6ebf22de1143162ca",
-    "url": "/rojhan-paydar-project-5/static/js/main.51a919a8.chunk.js"
+    "revision": "dd621300fa17479c791f",
+    "url": "/rojhan-paydar-project-5/static/js/main.1692e5d8.chunk.js"
   },
   {
     "revision": "df064b5c09af5da013c8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rojhan-paydar-project-5/precache-manifest.c40cfe3af6f85dbb5c4d08540f68b1f3.js"
+  "/rojhan-paydar-project-5/precache-manifest.5a7069287584d89970b60d263ef755a1.js"
 );
 
 self.addEventListener('message', (event) => {
