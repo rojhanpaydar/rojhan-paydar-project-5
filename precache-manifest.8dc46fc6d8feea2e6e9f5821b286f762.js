e3ecaa653d76d2370d4e1fd2be7528aa3d96270d
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-04 15:14:33 -0500

    Updates

---
+++ precache-manifest.8dc46fc6d8feea2e6e9f5821b286f762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d16a217d6e410254b5ce172be5013093",
+    "url": "/rojhan-paydar-project-5/index.html"
+  },
+  {
+    "revision": "5fb87df83f83a8e8b9d7",
+    "url": "/rojhan-paydar-project-5/static/css/main.cd536b39.chunk.css"
+  },
+  {
+    "revision": "35770f317209c999c9f6",
+    "url": "/rojhan-paydar-project-5/static/js/2.7d2715d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rojhan-paydar-project-5/static/js/2.7d2715d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fb87df83f83a8e8b9d7",
+    "url": "/rojhan-paydar-project-5/static/js/main.1692e5d8.chunk.js"
+  },
+  {
+    "revision": "df064b5c09af5da013c8",
+    "url": "/rojhan-paydar-project-5/static/js/runtime-main.2b7849a4.js"
+  }
+]);
