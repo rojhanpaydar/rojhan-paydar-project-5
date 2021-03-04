33f9f456c5ea08501a23fe0081579c01e5e5377f
Rojhan Paydar <rojhanpaydar@gmail.com>
2021-03-04 12:49:38 -0500

    made li tabbble, need to make them clickable via tabbing

--- src/App.js
+++ src/App.js
@@ -50,32 +50,32 @@ class App extends Component {
             <button onClick={this.getRestaurants}><span role="img" aria-label="emoji of french fries">🍟 </span>TIME FOR FOOD <span role="img" aria-label="emoji of sushi">🍣</span></button>
             {this.state.randomRestaurants ? (
               <ul>
-                <li>Restaurant: {this.state.randomRestaurants.name}</li>
+                <li tabindex="1">Restaurant: {this.state.randomRestaurants.name}</li>
                 <li>
                   <Image image={this.state.randomRestaurants} />
                 </li>
-                <li>
+                <li tabindex="2">
                   <h2>Cuisine:</h2> {this.state.randomRestaurants.cuisines}
                 </li>
-                <li>
+                <li tabindex="3">
                   <h2>Location:</h2> {this.state.randomRestaurants.location.address}
                 </li>
-                <li>
+                <li tabindex="4">
                   <h2>Area:</h2> {this.state.randomRestaurants.location.locality}
                 </li>
-                <li>
+                <li tabindex="5">
                   <h2>Hours:</h2> {this.state.randomRestaurants.timings}
                 </li>
-                <li>
+                <li tabindex="6">
                   <h2>Menu: </h2>{" "}
-                  <a href={this.state.randomRestaurants.menu_url}><span class="menuLink">CLICK HERE!</span></a>
+                  <a href={this.state.randomRestaurants.menu_url}><span class="linkStyles">CLICK HERE!</span></a>
                 </li>
-                <li>
+                <li tabindex="7">
                   <h2>Average Cost For Two:</h2> $
                   {this.state.randomRestaurants.average_cost_for_two}
                 </li>
-                <li>
-                  <h2>Phone Number:</h2> {this.state.randomRestaurants.phone_numbers}
+                <li tabindex="8">
+                  <h2>Phone Number:</h2> <a class="linkStyles" href="tel:">{this.state.randomRestaurants.phone_numbers}</a>
                 </li>
               </ul>
             ) : null}
